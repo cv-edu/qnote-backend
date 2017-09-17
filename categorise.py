e061ebdc8b8870c74d27fee98724fc1f6fdf0e09
jj534 <32003815+jj534@users.noreply.github.com>
2017-09-16 23:51:09 -0400

    Add files via upload

---
+++ categorise.py
@@ -0,0 +1,27 @@
+import url_to_text
+import http.client, urllib.request, urllib.parse, urllib.error, base64, requests, time, json
+
+def class_text(class_url):
+    '''
+    returns str of class name
+    '''
+    pass
+
+def date_text(date_url):
+    '''
+    returns str of date
+    '''
+    pass
+    
+def title_text(title_url):
+    '''
+    returns str of title
+    '''
+    pass
+
+def body_text(body_url):
+    '''
+    returns str of body
+    '''
+    pass
+
